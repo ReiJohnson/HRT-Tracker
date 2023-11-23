5b1e56d3fbf1142ad21e4a63ce91304415b1a13b
Rei Johnson <reirodneyjohnson@gmail.com>
2023-11-23 07:22:32 -0500

    Function definitions + console testing interface

--- tracker.py
+++ tracker.py
@@ -1,6 +1,4 @@
-import datetime
-import matplotlib.pyplot as plt
-import tkinter as tk
+import kivy
 
 mylanTransdermalAbArrY = [110, 120, 137.5, 112.5, 110, 105, 97.5, 92.5, 85, 80, 75, 72.5, 70, 67.5, 17.5, 5]
 mylanTransdermalAbArrX = [12, 24, 36, 48, 60, 72, 84, 96, 108, 120, 132, 144, 156, 168, 180, 192]
@@ -11,21 +9,27 @@ mylanTAAErrorMax = -10
 mylanTAAErrorMin = -2.5
 mylanTBAErrorMax = +15
 mylanTBAErrorMin = +2.5
-mylanTBAFitLine = None
+
+def seekRateNow(timeHours, loc):
+    if loc == 'ab':
+        valTime = mylanTransdermalAbArrX.index(min(mylanTransdermalAbArrX, key=lambda x:abs(x-timeHours)))
+        valUpper = mylanTransdermalAbArrY[valTime]
+        valLower = mylanTransdermalAbArrY[valTime-1]
+    else:
+        valTime = mylanTransdermalBtArrX.index(min(mylanTransdermalBtArrX, key=lambda x:abs(x-timeHours)))
+        valUpper = mylanTransdermalBtArrY[valTime]
+        valLower = mylanTransdermalBtArrY[valTime-1]
+
+    return (valUpper+valLower)/2            
 
 
-def modData(x):
-    yVals = []
-
-    for i in mylanTransdermalAbArrY:
-        erroredVal = lambda a : a+mylanTAAErrorMin if mylanTransdermalAbArrY.index(a) > 10 else a+2.5
-        yVals.append(erroredVal(i))
-
-    return plt.plot(x, yVals)
 
 
-mylanTAAFitLine = modData(mylanTransdermalAbArrX)
-mylanTBAFitLine = modData(mylanTransdermalBtArrX)
+startTimeD = input("How many days ago did you apply your patch: ")
+location = input("Location: ")
+timeDifferenceHours = int(startTimeD)*24
+daysToChange = 3.5-float(startTimeD)
 
-startTime = input("How many days ago did you apply your patch: ")
-timeDifferenceHours = startTime*24
+rateDiffOptimal = seekRateNow(timeDifferenceHours, 'ab' if location == 'abdomen' else 'bt')-97.5
+print('+'+str(rateDiffOptimal) if rateDiffOptimal > 0 else '-'+str(rateDiffOptimal))
+print('Reccomended change in: ' + str(daysToChange))
